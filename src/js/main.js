9c3582570fde41b44d05b1ef6383c6e6a5ca7b18
Lucas Flaquer <lucas.flaquer@vm2.com.br>
2019-04-17 11:39:38 -0300

    toggle top-bar

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,28 @@
 // js code here
+const body = document.querySelector('body');
 window.onload = function() {
 	
 	log();
+	toggleTopBar();
 };
 
 function log() {
 	console.log('ambiente pronto');
+}
+
+function toggleTopBar() {
+	let header = document.querySelector('.header');
+	let topBar = document.querySelector('.top-bar');
+	let toggle = document.querySelector('.top-bar> .toggle');
+	let hbody = header.offsetHeight;
+	toggle.addEventListener("click", function() {
+		topBar.classList.toggle("open");
+		if (topBar.classList.contains('open')) {
+			hbody += topBar.offsetHeight;
+		} else {
+			hbody -= topBar.offsetHeight;
+		}
+		body.style.paddingTop = `${hbody}px`;
+	});
+	body.style.paddingTop = `${hbody}px`;
 }
