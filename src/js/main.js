58d7ac51cff48fe6e4e198cf66e1a9f64054cfc7
lucasflaquer <lucas.flaquer@gmail.com>
2019-04-22 17:17:42 -0300

    header e ajustes de javascript

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,15 @@
 // js code here
 const body = document.querySelector('body');
 window.onload = function() {
-	
-	log();
 	toggleTopBar();
 };
-
-function log() {
-	console.log('ambiente pronto');
-}
 
 function toggleTopBar() {
 	let header = document.querySelector('.header');
 	let topBar = document.querySelector('.top-bar');
 	let toggle = document.querySelector('.top-bar> .toggle');
 	let hbody = header.offsetHeight;
+	body.style.paddingTop = `${hbody}px`;
 	toggle.addEventListener("click", function() {
 		topBar.classList.toggle("open");
 		if (topBar.classList.contains('open')) {
@@ -26,5 +21,4 @@ function toggleTopBar() {
 		}
 		body.style.paddingTop = `${hbody}px`;
 	});
-	body.style.paddingTop = `${hbody}px`;
 }
